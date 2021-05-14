ea9b25ca5404e0cc7db8a313886916082ff9cc35
dboren <danielboren@att.net>
2021-05-13 21:15:32 -0500

    added empty rows to table

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -5,20 +5,29 @@ function Main() {
         <div>
             <table border="1">
                 <thead>
-                    <tr>
+                    <tr border="1">
                         <th scope="col">Image</th>
                         <th scope="col">Name</th>
                         <th scope="col">Phone number</th>
                         <th scope="col">email address</th>
                         <th scope="col">DOB</th>
                     </tr>
-                    <tr>
+                    <tr border="1">
                         <td>Image will go here</td>
                         <td>Rusty Shackleford</td>
                         <td>777-777-7777</td>
                         <td>mockingbird@hotmail.com</td>
                         <td>10/16/1957</td>
                     </tr>
+                    <tr border="1"></tr>
+                    <tr border="1"></tr>
+                    <tr border="1"></tr>
+                    <tr border="1"></tr>
+                    <tr border="1"></tr>
+                    <tr border="1"></tr>
+                    <tr border="1"></tr>
+                    <tr border="1"></tr>
+                    <tr border="1"></tr>
                 </thead>
             </table>
         </div>
