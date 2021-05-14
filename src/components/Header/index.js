04320ec41b0c398b778a02c62deee7244ee44e7f
dboren <danielboren@att.net>
2021-05-13 20:11:26 -0500

    added header text to Header compontent

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <h1>Employee Directory</h1>
+
+            <h4>Click the column titles to filter by column or use the search bar to narrow your results</h4>
+        </div>
+    )
+}
+
+export default Header;
