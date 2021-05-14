79fa6471dceeb8cb881ac56f9897d7e6797a3db4
dboren <danielboren@att.net>
2021-05-13 20:13:03 -0500

    added result limit of 10 to api call

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     getRandomPerson: function() {
-      return axios.get("https://randomuser.me/api/");
+      return axios.get("https://randomuser.me/api/?results=10");
     }
 };
