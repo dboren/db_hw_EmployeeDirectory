8bd70bbfbded2e52a4382176f073d5a5a198a8e3
dboren <danielboren@att.net>
2021-05-13 22:14:20 -0500

    refactored Main as class based component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,39 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import EmployeeData from "../EmployeeData/index"
 import API from "../../utils/API";
 
-function Main() {
-    return (
-        <div>
-            <table border="1">
-                <thead>
-                    <tr border="1">
-                        <th scope="col">Image</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Phone number</th>
-                        <th scope="col">email address</th>
-                        <th scope="col">DOB</th>
-                    </tr>
-                    <tr border="1">
-                        <td>Image will go here</td>
-                        <td>Rusty Shackleford</td>
-                        <td>777-777-7777</td>
-                        <td>mockingbird@hotmail.com</td>
-                        <td>10/16/1957</td>
-                    </tr>
-                    <tr border="1"></tr>
-                    <tr border="1"></tr>
-                    <tr border="1"></tr>
-                    <tr border="1"></tr>
-                    <tr border="1"></tr>
-                    <tr border="1"></tr>
-                    <tr border="1"></tr>
-                    <tr border="1"></tr>
-                    <tr border="1"></tr>
-                </thead>
-            </table>
-        </div>
-    )
+class Main extends Component {
+    
+    render() {
+        return (
+            <div>
+                <table border="1">
+                    <thead>
+                        <tr border="1">
+                            <th scope="col">Image</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Phone number</th>
+                            <th scope="col">email address</th>
+                            <th scope="col">DOB</th>
+                        </tr>
+                        <tr border="1">
+                            <td>Image will go here</td>
+                            <td>Rusty Shackleford</td>
+                            <td>777-777-7777</td>
+                            <td>mockingbird@hotmail.com</td>
+                            <td>10/16/1957</td>
+                        </tr>
+                        <tr border="1"></tr>
+                        <tr border="1"></tr>
+                        <tr border="1"></tr>
+                        <tr border="1"></tr>
+                        <tr border="1"></tr>
+                        <tr border="1"></tr>
+                        <tr border="1"></tr>
+                        <tr border="1"></tr>
+                        <tr border="1"></tr>
+                    </thead>
+                </table>
+            </div>
+            
+        )}
 }
 
 export default Main;
