f31980ea00992d991b7bad9066a726fafce71f37
dboren <danielboren@att.net>
2021-05-13 21:56:27 -0500

    added API search results to EmployeeData component

--- src/components/EmployeeData/index.js
+++ src/components/EmployeeData/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-
+function EmployeeData(props) {
+    return (
+        <div>
+            <img src={props.results.picture.thumbnail}></img>
+            <p>{props.results.name.first} {props.results.name.last}</p>
+            <p>{props.results.phone}</p>
+            <p>{props.results.email}</p>
+            <p>{props.results.dod}</p>
+         </div>
+    );
+    }
 
 export default EmployeeData;
